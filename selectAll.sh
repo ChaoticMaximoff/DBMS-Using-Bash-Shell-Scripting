d1f1b258fb271b8897a31d61b6cf8ea199c406e1
Mazen <ahedmazenm@outlook.com>
2025-01-24 10:28:33 +0200

    selectall now appear as table

--- selectAll.sh
+++ selectAll.sh
@@ -11,7 +11,15 @@ if [[ -f $table ]]; then
     zenity --info --title="Table Found" --text="Displaying all records from '$table':"
     
     # Process and format the metadata as a table
-    table_content=$(cut -d: -f1 .$table-metadata)
+table_content=$(awk -F: '
+    BEGIN {
+        printf "%-15s %-10s %-10s\n", "Field Name", "Data Type", "Primary Key"
+        printf "%-20s %-20s %-20s\n", "-------------------", "-------------------", "-------------------"
+    }
+    {
+        printf "%-30s %-20s %-20s\n", $1, $2, $3
+    }
+' .$table-metadata)
 
     # Display the formatted table using zenity
     zenity --text-info \

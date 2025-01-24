501ef5627ac679b8e13e0883272297e3b05a8e5c
ChaoticMaximoff <hagargalalstu@gmail.com>
2025-01-24 21:12:35 +0200

    added insert into table functionality

--- insert.sh
+++ insert.sh
@@ -17,11 +17,11 @@ while true; do
                 --title="Table Selected" \
                 --text="The selected table is: $insertInTB"
 
-            colSize=`wc -l .$1-metadata | cut -d" " -f1`
+            colSize=`wc -l .$insertInTB-metadata | cut -d" " -f1`
             rowData=""
 
             for (( i=1; i<=$colSize; i++ )); do
-                line=`sed -n "$(echo $i)p" .$1-metadata`
+                line=`sed -n "$(echo $i)p" .$insertInTB-metadata`
                 colName=`echo $line | cut -d: -f1`
 				colType=`echo $line | cut -d: -f2`
 				colPKCheck=`echo $line | cut -d: -f3`
@@ -30,8 +30,44 @@ while true; do
                     --text="Enter data for $colName ($colType):" \
                     --entry-text="")
                 
-                
+                if [[ $colType == "int" ]]; then
+                    while ! [[ $colData =~ ^-?[0-9]*$ ]]; do
+                        zenity --error \
+                            --title="Invalid Value" \
+                            --text="The value must be an integer.\n\nTry again."
+                        colData=$(zenity --entry \
+                            --title="Insert Data into $insertInTB" \
+                            --text="Enter data for $colName ($colType):" \
+                            --entry-text="")
+                    done
+
+
+                fi
+
+                if [[ $colPKCheck = "pk" ]]; then
+
+                    if [[ $colData == "" ]]; then
+                        zenity --error \
+                            --title="Empty Value" \
+                            --text="The primary key column cannot be empty.\n\nPlease enter a value."
+                            return  # Continue the outer loop
+                    fi
+
+                    allValues=($(awk -F: '{print $'"$i"'}' ./$insertInTB))
+                    for val in "${allValues[@]}"; do
+                        if [[ $val == $colData ]]; then
+                            zenity --error \
+                                --title="Duplicate Value" \
+                                --text="The value $colData already exists in the table.\n\nPlease enter a unique value."
+                            return  # Continue the outer loop
+                        fi
+                    done
+                fi
+
+                rowData+=$colData:
+
             done
+            echo $rowData >> $insertInTB
             
             
         else
